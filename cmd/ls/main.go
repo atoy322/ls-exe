4fb4b8e65ae536d2873da0f0e025ecfb906ffb59
atoy322 <atoy_0322@yahoo.co.jp>
2022-02-12 16:09:11 +0900

    edit main.go

--- cmd/ls/main.go
+++ cmd/ls/main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"io/fs"
+	"os"
 
-    "github.com/atoy322/ls-exe/internal/colorization"
+	"github.com/atoy322/ls-exe/internal/cmdline_parser"
+	"github.com/atoy322/ls-exe/internal/colorization"
+	"github.com/atoy322/ls-exe/internal/pretty_print"
 )
 
 func main() {
-    ls, e := os.ReadDir(".")
+    options := cmdline_parser.Parse(os.Args)
+    // options.Color = true
+    fmt.Println(options)
+    dir := options.Directory
+    finfo, e := os.Stat(dir)
+
+    ls := make([]os.DirEntry, 0)
     if e != nil {
-        panic(e)
+        fmt.Println("ls: No such file or directory.")
+    } else if !finfo.IsDir() {  // is file
+        ls = append(ls, fs.FileInfoToDirEntry(finfo))
+    } else {  // is folder
+        ls, e = os.ReadDir(dir)
+
+        if e != nil {
+            panic(e)
+        }
     }
+
     style := &colorization.TextStyle{}
+    filenames := make([]string, 0)
     for _, i := range ls {
         style = colorization.Judge(i.Type())
-        fmt.Println(style.Colorize(i.Name()))
+        filenames = append(filenames, style.Colorize(i.Name()))
     }
+
+    pretty_print.Pprint(filenames)
 }
